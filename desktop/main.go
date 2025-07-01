2cc0cae7ddbe70fa3e4025c3a1bf0007c8f938ba
Benjamin Smith <ben.smith.developer@gmail.com>
2025-07-01 19:55:37 -0400

    update Header

--- desktop/main.go
+++ desktop/main.go
@@ -19,7 +19,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "desktop",
+		Title:  "Atmos Desktop",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
